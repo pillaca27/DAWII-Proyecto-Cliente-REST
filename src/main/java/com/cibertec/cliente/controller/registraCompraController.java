dc4cf4d225d392a46d198a285d731507cba21be8
pillaca27 <cristal_27.2011@hotmail.com>
2021-11-25 20:14:41 -0500

    listo

--- src/main/java/com/cibertec/cliente/controller/registraCompraController.java
+++ src/main/java/com/cibertec/cliente/controller/registraCompraController.java
@@ -1,15 +1,19 @@
 package com.cibertec.cliente.controller;
 
+import java.io.FileInputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -23,6 +27,14 @@ import com.cibertec.cliente.entity.Venta;
 import com.cibertec.cliente.entity.VentaDetalle;
 import com.cibertec.cliente.entity.VentaDetallePK;
 import com.google.gson.Gson;
+
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 
 @Controller
 @RequestMapping("/Compra")
@@ -115,6 +127,41 @@ public class registraCompraController {
 		seleccionados.clear();
 		return "redirect:/Compra/registro";
 	
+	}
+	
+	@RequestMapping("/PDF")
+	public ResponseEntity<byte[]> index(@RequestParam String codigo, @RequestParam String proveedor, 
+			                            @RequestParam String empleado, @RequestParam String fecha_emision, 
+			                            @RequestParam String fecha_reparto, @RequestParam String fecha_vencimiento,
+			                            @RequestParam String tipo_compra, @RequestParam String tipo_pago) throws Exception, JRException {
+		double monto = 0;
+		for(Seleccion2 a : seleccionados) {
+			monto += a.getCantidad() * a.getPrecio();
+		}
+		
+		JRBeanCollectionDataSource jb = new JRBeanCollectionDataSource(seleccionados);
+		JasperReport compileReport = JasperCompileManager.compileReport(new FileInputStream("src/main/resources/compra.jrxml"));
+		
+		HashMap<String, Object> map = new HashMap<>();
+		map.put("monto", monto);
+		map.put("codigo", codigo);
+		map.put("proveedor", proveedor);
+		map.put("empleado", empleado);
+		map.put("fecha_emision",fecha_emision);
+		map.put("fecha_reparto",fecha_reparto);
+		map.put("fecha_vencimiento",fecha_vencimiento);
+		map.put("tipo_compra",tipo_compra);
+		map.put("tipo_pago",tipo_pago);
+		
+		JasperPrint repot = JasperFillManager.fillReport(compileReport, map, jb);
+		JasperExportManager.exportReportToPdfFile(repot, "compra_"+codigo+".pdf");
+		
+		byte[] data = JasperExportManager.exportReportToPdf(repot);
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.set(HttpHeaders.CONTENT_DISPOSITION, "inline;filename=compra_"+codigo+".pdf");
+		
+		return ResponseEntity.ok().headers(headers).contentType(MediaType.APPLICATION_PDF).body(data);
 	}
 	
 
